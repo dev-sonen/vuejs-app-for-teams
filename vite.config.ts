c1dc3464198896cc1fb3ca619f1b59f9502e4bab
dev-sonen <dev.bensondimasupil@gmail.com>
2025-01-07 17:48:08 +0800

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig( {
     plugins: [
         vue(),
         vueJsx(),
-        vueDevTools()
+        vueDevTools(),
+        mkcert()
     ],
     server: {
         port: 3000             
